0c49aff38c4bac746eb3f5e7577a10ffd9983237
Isabel Maccabee <isabel.maccabee@northcoders.com>
2020-12-15 10:28:04 +0000

    move cablecar to the ground

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const useOctoFunicular = ()=> {
-    console.log('the curliest cablecar ever with 8 tentacles!')
+    console.log('the curliest ground cablecar ever with 8 tentacles!')
+    console.log('the floor looks amazing from down here')
 }
